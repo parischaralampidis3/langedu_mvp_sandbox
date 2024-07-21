6f5ccac64fee1b51c07f185b2d065d2428ad7a81
@parou.char <curlycode@outlook.com>
2024-07-22 01:00:33 +0300

    refactoring code git push

--- tms/dashboard/urls.py
+++ tms/dashboard/urls.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+
 from django.urls import path
 from .views import home
+from .views import users
 urlpatterns = [
     path('', home, name='home'),
+    path('users/', users, name='users'),
 ]
--- tms/dashboard/views.py
+++ tms/dashboard/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def users(request):
+    return render(request, 'users.html')
+
