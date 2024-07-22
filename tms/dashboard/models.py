92915ab1c1b3be982100b92a701c94ed63554829
@parou.char <curlycode@outlook.com>
2024-07-22 22:19:02 +0300

    define model and view

--- tms/dashboard/models.py
+++ tms/dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=50)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now= True)
+
+
--- tms/dashboard/views.py
+++ tms/dashboard/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import User
+
 def home(request):
     return render(request, 'index.html')
 
 def users(request):
     return render(request, 'users.html')
+
+def index_users(request):
+    users_list = User.objects.all()
+    return render(request, 'templates/users.html', {'users_list': users_list})
 
