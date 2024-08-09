c84ab539cf4875d634160ac5958231232c2a9fb8
@parou.char <curlycode@outlook.com>
2024-08-09 10:22:35 +0300

    commit course model

--- tms/dashboard/models.py
+++ tms/dashboard/models.py
@@ -2,15 +2,33 @@ from django.db import models
 
 from django.db import models
 
-
-class Student(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    dob = models.DateField()
-    email = models.EmailField(max_length=50)
-    username = models.CharField(max_length=100)
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_enabled = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+class Student(models.Model):
+    username = models.CharField(max_length=100)
+    course_id = models.ManyToManyField(Course)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=50)
+    dob = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_enabled = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['email']
 
     def __str__(self):
         return self.email
