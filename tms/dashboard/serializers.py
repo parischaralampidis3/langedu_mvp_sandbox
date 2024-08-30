1b1259cf5371a773973f0efccb20388986c5bf6b
@parou.char <curlycode@outlook.com>
2024-08-30 13:27:19 +0300

    commit course rest api

--- tms/dashboard/serializers.py
+++ tms/dashboard/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Course
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'dob', 'created_on',
                   'updated_on']
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = ['id', 'title']
