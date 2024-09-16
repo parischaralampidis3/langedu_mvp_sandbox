527b8360f03eaba99ea031f64b25d037adf86aa1
@parou.char <curlycode@outlook.com>
2024-09-16 13:12:37 +0300

    refactoring courses view

--- tms/dashboard/urls/__init__.py
+++ tms/dashboard/urls/__init__.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('',home, name='home'),
     path('students/', include('dashboard.urls.students')),  # Correctly include the students URLs
+    path('courses/', include('dashboard.urls.courses')),  # Correctly include the students URLs
 ]
---
+++ tms/dashboard/urls/courses.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from ..views import courses, course,create_course,update_course,delete_course,enroll_lesson
+
+urlpatterns = [
+    path('courses', courses, name='courses'),
+    path('<int:id>/', course, name='course'),
+    path('create_course', create_course, name='create_course'),
+    path('<int:id>/', update_course, name='update_course'),
+    path('<int:id>/', delete_course, name='delete_course'),
+    path('enroll_lesson',enroll_lesson, name='enroll_lesson')
+]
